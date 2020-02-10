3a775c17108169fab9cef92dfb976af35ac7b8e8
DaoQuangPhuong <daoquangphuong@gmail.com>
2020-02-10 13:50:22 +0700

    alignment style

--- src/sectors/paragraph.js
+++ src/sectors/paragraph.js
@@ -10,25 +10,29 @@ const paragraph = {
       defaults: "left",
       list: [
         {
+          title: "Left Align",
           value: "left",
           name: "Left",
           className: "fa fa-align-left"
         },
         {
+          title: "Center Align",
           value: "center",
           name: "Center",
           className: "fa fa-align-center"
         },
         {
+          title: "Justify Align",
+          value: "justify",
+          name: "Justify",
+          className: "fa fa-align-justify"
+        },
+        {
+          title: "Right Align",
           value: "right",
           name: "Right",
           className: "fa fa-align-right"
         },
-        {
-          value: "justify",
-          name: "Justify",
-          className: "fa fa-align-justify"
-        }
       ],
       toRequire: 1
     },
@@ -37,18 +41,28 @@ const paragraph = {
       name: "Style",
       property: "list-style-type",
       type: "radio",
-      defaults: "left",
+      defaults: "",
       list: [
         {
-          value: "disc",
+          value: "bulleted",
           name: "Bulleted List",
           className: "fa fa-list-ul"
         },
         {
-          value: "decimal",
+          value: "numbered",
           name: "Numbered List",
           className: "fa fa-list-ol"
-        }
+        },
+        {
+          value: "decrease",
+          name: "Decrease Indent",
+          className: "fa fa-indent"
+        },
+        {
+          value: "increase",
+          name: "Increase Indent",
+          className: "fa fa-outdent"
+        },
       ],
       toRequire: 1
     }
