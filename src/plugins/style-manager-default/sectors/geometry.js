2109618ea93c94ea1cda94c2e580fad75006d7f5
DaoQuangPhuong <daoquangphuong@gmail.com>
2020-02-12 15:05:48 +0700

    update geometry

--- src/plugins/style-manager-default/sectors/geometry.js
+++ src/plugins/style-manager-default/sectors/geometry.js
@@ -1,28 +1,46 @@
 const geometry = {
     name: "Geometry",
-    buildProps: ["width", "height", "left", "top", "right", "bottom", "display", "position", "float", "max-width",
+    buildProps: [
+        "float",
+        "display",
+        "position",
+        "max-width",
         "min-height",
+        "width",
+        "height",
+        "left",
+        "top",
+        "right",
+        "bottom",
         "margin",
-        "padding"],
+        "padding"
+    ],
     properties: [
+        {property: 'position', type: 'select'},
         {
             name: "Margin",
             property: "margin",
             properties: [
-                {name: "Top", property: "margin-top", type: "slider", max: 1024},
-                {name: "Right", property: "margin-right", type: "slider", max: 1024},
-                {name: "Bottom", property: "margin-bottom", type: "slider", max: 1024},
-                {name: "Left", property: "margin-left", type: "slider", max: 1024}
+                {classList: ["sl-inline-slider"], name: "Top", property: "margin-top", type: "slider", max: 1024},
+                {classList: ["sl-inline-slider"], name: "Right", property: "margin-right", type: "slider", max: 1024},
+                {classList: ["sl-inline-slider"], name: "Bottom", property: "margin-bottom", type: "slider", max: 1024},
+                {classList: ["sl-inline-slider"], name: "Left", property: "margin-left", type: "slider", max: 1024}
             ]
         },
         {
             name: "Padding",
             property: "padding",
             properties: [
-                {name: "Top", property: "padding-top", type: "slider", max: 1024},
-                {name: "Right", property: "padding-right", type: "slider", max: 1024},
-                {name: "Bottom", property: "padding-bottom", type: "slider", max: 1024},
-                {name: "Left", property: "padding-left", type: "slider", max: 1024}
+                {classList: ["sl-inline-slider"], name: "Top", property: "padding-top", type: "slider", max: 1024},
+                {classList: ["sl-inline-slider"], name: "Right", property: "padding-right", type: "slider", max: 1024},
+                {
+                    classList: ["sl-inline-slider"],
+                    name: "Bottom",
+                    property: "padding-bottom",
+                    type: "slider",
+                    max: 1024
+                },
+                {classList: ["sl-inline-slider"], name: "Left", property: "padding-left", type: "slider", max: 1024}
             ]
         }
     ]
