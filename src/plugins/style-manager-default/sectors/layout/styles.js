90383af6a00094fe3dddea8b0cdb859abd4dfc9a
Hoa Tran <hoa.thanh.tran@dfo.global>
2020-02-26 18:13:16 +0700

    Update Box Alignment

--- src/plugins/style-manager-default/sectors/layout/styles.js
+++ src/plugins/style-manager-default/sectors/layout/styles.js
@@ -15,38 +15,32 @@ export default (editor, opts = {}) => {
           {
             title: "Left Align",
             value: "left",
-            name: "Left",
-            className: "fa fa-align-left"
+            name: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#000" fill-rule="nonzero" d="M6 6v12h13V6H6zm0 13v5H5V0h1v5h13c.6 0 1 .4 1 1v12c0 .6-.4 1-1 1H6zm6-14h1v14h-1V5z"/></svg>'
           },
           {
             title: "Center Align",
             value: "center",
-            name: "Center",
-            className: "fa fa-align-center"
+            name: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#000" fill-rule="nonzero" d="M6 6v12h13V6H6zm0-1h13c.6 0 1 .4 1 1v12c0 .6-.4 1-1 1H6a1 1 0 01-1-1V6c0-.6.4-1 1-1zm6-5h1v24h-1V0z"/></svg>'
           },
           {
             title: "Right Align",
             value: "right",
-            name: "Right",
-            className: "fa fa-align-right"
+            name: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#000" fill-rule="nonzero" d="M6 6v12h13V6H6zm13-1V0h1v24h-1v-5H6a1 1 0 01-1-1V6c0-.6.4-1 1-1h13zm-7 0h1v14h-1V5z"/></svg>'
           },
           {
             title: "Top Align",
             value: "top",
-            name: "Top",
-            className: "fa fa-align-left"
+            name: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#000" fill-rule="nonzero" d="M18 6H6v13h12V6zM5 6H0V5h24v1h-5v13c0 .6-.4 1-1 1H6a1 1 0 01-1-1V6zm14 6v1H5v-1h14z"/></svg>'
           },
           {
             title: "Middle Align",
             value: "middle",
-            name: "Middle",
-            className: "fa fa-align-center"
+            name: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#000" fill-rule="nonzero" d="M18 6H6v13h12V6zm1 0v13c0 .6-.4 1-1 1H6a1 1 0 01-1-1V6c0-.6.4-1 1-1h12c.6 0 1 .4 1 1zm5 6v1H0v-1h24z"/></svg>'
           },
           {
             title: "Bottom Align",
             value: "bottom",
-            name: "Bottom",
-            className: "fa fa-align-right"
+            name: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#000" fill-rule="nonzero" d="M18 19H6V6h12v13zM5 19H0v1h24v-1h-5V6c0-.6-.4-1-1-1H6a1 1 0 00-1 1v13zm14-6v-1H5v1h14z"/></svg>'
           },
         ]
       })
@@ -96,52 +90,49 @@ export default (editor, opts = {}) => {
           return this.getValidParent(parent, parent.parent());
         }
       },
+      updatePosition(x, y) {
+        const sm = editor.StyleManager;
+        const leftProp = sm.getProperty('geometry','left');
+        const topProp = sm.getProperty('geometry','top');
+
+        leftProp.set('value', x);
+        topProp.set('value', y);
+      },
       handleAlignment(action){
         const selected = editor.getSelected();
         const validTarget = this.getValidParent(selected, selected.parent(), action);
         const box = validTarget.target;
-        box.addStyle({ position: 'relative', transform:  'translateX(0) translateY(0)'});
         const { left, right, top, bottom, center, middle } = this.getCoords(
           validTarget.parent.getEl(),
           box.getEl()
         );
 
-        const sm = editor.StyleManager;
-        const l = sm.getProperty('geometry','left');
-        const t = sm.getProperty('geometry','top');
+        if(validTarget.parent.getStyle().position === "static") {
+          validTarget.parent.addStyle({ position: "relative" });
+        }
+
+        box.addStyle({ transform:  'translateX(0) translateY(0)'});
 
         const currentTop = box.getStyle().top;
         const currentLeft = box.getStyle().left;
         switch (action) {
           case "left":
-            //box.addStyle({ top: currentTop, left })
-            l.set('value', left);
-            t.set('value', currentTop);
+            this.updatePosition(left, currentTop)
             break;
           case "center":
-            //box.addStyle({ top: currentTop, left: center })
-            l.set('value', center);
-            t.set('value', currentTop);
+            this.updatePosition(center, currentTop)
             break;
           case "right":
-            //box.addStyle({ top: currentTop, left: right })
-            l.set('value', right);
-            t.set('value', currentTop);
+            this.updatePosition(right, currentTop)
             break;
           case "top":
-            //box.addStyle({ top: top, left: currentLeft })
-            l.set('value', currentLeft);
-            t.set('value', top);
+            this.updatePosition(currentLeft, top)
             break;
           case "middle":
-            //box.addStyle({ top: middle, left: currentLeft })
-            l.set('value', currentLeft);
-            t.set('value', middle);
+            this.updatePosition(currentLeft, middle)
             break;
           case "bottom":
-            //box.addStyle({ top: bottom, left: currentLeft })
-            l.set('value', currentLeft);
-            t.set('value', bottom);
+            this.updatePosition(currentLeft, bottom)
             break;
         }
         editor.trigger('component:toggled')
