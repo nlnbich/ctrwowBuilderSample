60d703ddbf04584d60a21d00d23fc09d47939957
Hoa Tran <hoa.thanh.tran@dfo.global>
2020-02-27 18:23:43 +0700

    Implement Full Width action

--- src/plugins/style-manager-default/sectors/layout/styles.js
+++ src/plugins/style-manager-default/sectors/layout/styles.js
@@ -47,6 +47,12 @@ export default (editor, opts = {}) => {
             value: 'bottom',
             name:
               '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#000" fill-rule="nonzero" d="M18 19H6V6h12v13zM5 19H0v1h24v-1h-5V6c0-.6-.4-1-1-1H6a1 1 0 00-1 1v13zm14-6v-1H5v1h14z"/></svg>'
+          },
+          {
+            title: 'Full Width',
+            value: 'full-width',
+            name:
+              '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#000" fill-rule="evenodd" d="M2 6v12h6V6H2zm0 13v5H1V0h1v5h6c.6 0 1 .4 1 1v12c0 .6-.4 1-1 1H2zM16 6v12h6V6h-6zm6-1V0h1v24h-1v-5h-6a1 1 0 01-1-1V6c0-.6.4-1 1-1h6z"></path></svg>'
           }
         ]
       })
@@ -75,9 +81,18 @@ export default (editor, opts = {}) => {
           top: 0 - padding.top + 'px',
           left: 0 - padding.left + 'px',
           right: elem.clientWidth - children.clientWidth - padding.left + 'px',
-          bottom: elem.clientHeight - children.clientHeight - padding.top + 'px',
-          center: elem.clientWidth / 2 - children.clientWidth / 2 - padding.left + 'px',
-          middle: elem.clientHeight / 2 - children.clientHeight / 2 - padding.top + 'px'
+          bottom:
+            elem.clientHeight - children.clientHeight - padding.top + 'px',
+          center:
+            elem.clientWidth / 2 -
+            children.clientWidth / 2 -
+            padding.left +
+            'px',
+          middle:
+            elem.clientHeight / 2 -
+            children.clientHeight / 2 -
+            padding.top +
+            'px'
         }
       },
       getValidParent(target, parent, position) {
@@ -85,10 +100,18 @@ export default (editor, opts = {}) => {
         let $parent = parent.getEl()
         let i = 0
         let valid = $parent.clientWidth - $target.clientWidth >= PADDING
-        if (position === 'top' || position === 'bottom' || position === 'middle') {
+        const grandParent = parent.parent()
+        if (
+          position === 'top' ||
+          position === 'bottom' ||
+          position === 'middle'
+        ) {
           valid = $parent.clientHeight - $target.clientHeight >= PADDING
         }
-        if (valid || i === LIMIT_LEVEL) {
+        if (valid ||
+          !grandParent ||
+          grandParent.get('type') === 'wrapper' ||
+          i === LIMIT_LEVEL) {
           return { target, parent }
         } else {
           return this.getValidParent(parent, parent.parent())
@@ -99,18 +122,23 @@ export default (editor, opts = {}) => {
         const leftProp = sm.getProperty('geometry', 'left')
         const topProp = sm.getProperty('geometry', 'top')
 
-        leftProp.set('value', x)
-        topProp.set('value', y)
+        leftProp.set('value', x ? x : 0)
+        topProp.set('value', y ? y : 0)
       },
       handleAlignment(action) {
-        const selected = editor.getSelected()
-        const validTarget = this.getValidParent(selected, selected.parent(), action)
-        const box = validTarget.target
-        const { left, right, top, bottom, center, middle } = this.getCoords(validTarget.parent.getEl(), box.getEl())
+        const box = editor.getSelected()
+        const parent = box.parent()
 
-        const parentStyle = validTarget.parent.getStyle()
+        if (!parent) return
+
+        const { left, right, top, bottom, center, middle } = this.getCoords(
+          parent.getEl(),
+          box.getEl()
+        )
+
+        const parentStyle = parent.getStyle()
         if (!parentStyle.position || parentStyle.position === 'static') {
-          validTarget.parent.addStyle({ position: 'relative' })
+          parent.addStyle({ position: 'relative' })
         }
 
         box.addStyle({ transform: 'translateX(0) translateY(0)' })
@@ -136,6 +164,10 @@ export default (editor, opts = {}) => {
           case 'bottom':
             this.updatePosition(currentLeft, bottom)
             break
+          case 'full-width':
+            this.updatePosition(left, currentTop)
+            box.addStyle({ width: '100%', 'max-width': '100%' })
+            break
         }
         editor.trigger('component:toggled')
       },
@@ -143,7 +175,8 @@ export default (editor, opts = {}) => {
         e && e.stopPropagation()
         const align = this.getInputValue()
         this.handleAlignment(align)
+        this.model.set('value', '')
       }
     })
   })
-}
+};
